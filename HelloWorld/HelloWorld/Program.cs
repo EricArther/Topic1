3dfe2bee4ae91a53edae0970a6b50d848c244d1d
Ayumu Nesfield <30004313@student.toiohomai.ac.nz>
2018-07-26 13:29:24 +1200

    201807260129

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "Hello World";
+            Console.WriteLine(output);
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
